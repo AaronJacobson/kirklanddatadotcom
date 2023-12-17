a9034e6377f6d8ce573a7405a2a3a0cd0e4d1914
Aaron Jacobson <aaronlj@uw.edu>
2023-12-17 15:10:38 -0800

    replacing hello

--- main_view/apps.py
+++ main_view/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloConfig(AppConfig):
+class MainViewConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "hello"
+    name = "main_view"
