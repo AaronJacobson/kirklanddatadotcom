2306ca57151518181cc190c19869af269d7a0237
Aaron Jacobson <aaron.le.jacobson@gmail.com>
2023-12-17 22:45:18 -0800

    Added google group for website updates

--- pages/home.py
+++ pages/home.py
@@ -9,7 +9,7 @@ layout = html.Div(
             """
             Kirkland Data is a place for hosting interesting data analyses and projects relating to the Kirkland, Washington and the surrounding area.
 
-            This website is built and maintained by Kirkland resident Aaron Jacobson.
+            This website is built and maintained by Kirkland resident Aaron Jacobson. You can sign up for email alerts for whenever this site is updated by joining this google group: [https://groups.google.com/g/kirklanddata-updates](https://groups.google.com/g/kirklanddata-updates)
 
             You can get in contact with Aaron by:
             
